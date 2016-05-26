267df44b60110e9be150926fcbbed8150564d568
zonblar <ayouche.mehdi@gmail.com>
2016-05-26 15:06:05 +0200

    commit level 1

--- backend/level1/main.rb
+++ backend/level1/main.rb
@@ -1,3 +1,31 @@
 require "json"
+require "date"
 
 # your code
+
+def rental_prices
+   # parsing the json
+   file = File.read('data.json')
+   input_hash = JSON.parse(file)
+   # defining the ouptu array where we'll stock the hash objects
+   output_array = []
+   # iterating over the rentals array of the input hash to return json with id and price
+   input_hash["rentals"].each do |rental|
+   # iterating over the cars array of the input hash to get the associated car for each rental
+     associated_car = {}
+     input_hash["cars"].each do |car|
+      if car["id"] == rental["car_id"]
+         associated_car = car
+      end
+     end
+     ending = Date.parse(rental["end_date"])
+     starting = Date.parse(rental["start_date"])
+      output_array << {
+        "id": rental["id"],
+        "price": (1+(ending - starting).to_i) * associated_car["price_per_day"] + rental["distance"] * associated_car["price_per_km"]
+        }
+   end
+
+   output_hash = { "rentals": output_array }
+ end
+
